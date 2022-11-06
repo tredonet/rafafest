062e95e742290d3515da3ad7fee09b346025f0f7
Tonino Redonet <toninoredonet@gmail.com>
2022-11-06 21:02:16 +0100

    simple landing page

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,91 +1,74 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import { usePreferredColorScheme } from "@vueuse/core";
+import HomeView from "./views/HomeView.vue";
+import imageWhite from "@/assets/img/IMG_0178_th_white.png";
+import imageBlack from "@/assets/img/IMG_0178_th_black.png";
+
+const preferredColor = usePreferredColorScheme();
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="wrapper">
+    <div class="imageWrapper">
+      <img
+        class="image"
+        :src="preferredColor === 'light' ? imageBlack : imageWhite"
+      />
     </div>
-  </header>
-
-  <RouterView />
+    <div class="textWrapper">
+      <HomeView />
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.image {
+  opacity: 0.1;
+  width: 150vw;
+  overflow: hidden;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.imageWrapper {
+  position: absolute;
+  top: 10vh;
+  left: -25vw;
+  overflow: hidden;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
+.textWrapper {
+  overflow: hidden;
+  position: absolute;
+  left: 33vw;
+  top: 35vh;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .image {
+    opacity: 0.5;
+    height: 100vh;
+    width: 90vh;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .imageWrapper {
+    position: absolute;
+    left: 45vh;
+    top: 0;
+    height: 100vh;
+    width: 45vh;
+    overflow: hidden;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  .textWrapper {
+    height: 100vh;
+    position: absolute;
+    left: 58vw;
+    top: 30vh;
   }
 }
 </style>
--- apps/frontend/src/views/HomeView.vue
+++ apps/frontend/src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="title">RAFAFEST</h1>
+    <h1 class="date">15.04.23</h1>
+    <h1>SAVETHEDATE</h1>
   </main>
 </template>
+
+<style>
+h1 {
+  font-family: Montserrat, sans-serif;
+  font-weight: 100;
+  font-size: 5vw;
+  line-height: 4vw;
+}
+
+.title {
+  font-size: 7vw;
+  line-height: 5.2vw;
+}
+
+.date {
+  font-size: 8vw;
+  line-height: 7.5vw;
+}
+</style>
